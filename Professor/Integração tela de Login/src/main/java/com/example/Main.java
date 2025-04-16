2b89850b516408b89de4db648b519af638c79932
Pedro Vinicius de Morais Capelari <pedro_capelari@hotmail.com>
2025-04-15 22:14:16 -0300

    15/04/2025

--- Professor/Integração tela de Login/src/main/java/com/example/Main.java
+++ Professor/Integração tela de Login/src/main/java/com/example/Main.java
@@ -2,7 +2,8 @@ package com.example;
 
 public class Main {
     
-    public static void main(String[] args) {
+    public static void main(String args[]){
         App.main(args);
+        
     }
 }
