6193f9978bb450cbb07aead548e442582ce64e5e
fabiane13 <faedu3@hotmail.com>
2025-04-23 19:45:14 -0300

    rh

--- RH/gestaoderh/src/main/java/com/example/controllers/DadoPessoalController.java
+++ RH/gestaoderh/src/main/java/com/example/controllers/DadoPessoalController.java
@@ -9,7 +9,6 @@ import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
 
 
 import java.sql.*;
